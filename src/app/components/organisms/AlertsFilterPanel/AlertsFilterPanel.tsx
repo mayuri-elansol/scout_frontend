7494e915da1a76a5f60a89bd7b862489f846099b
mayuri-elansol <mayurib@elansoltech.com>
2025-09-08 15:38:49 +0530

    pull commit

--- src/app/components/organisms/AlertsFilterPanel/AlertsFilterPanel.tsx
+++ src/app/components/organisms/AlertsFilterPanel/AlertsFilterPanel.tsx
@@ -15,7 +15,7 @@ import { Refresh, Check } from "@mui/icons-material";
 import {
   getCategoryOptions,
   getSubCategoriesForCategory,
-} from "@/app/config/alertsFilterConfig";
+} from "@/app/config/menuConfig";
 
 interface AlertsFilterPanelProps {
   categoryFilter?: string;
