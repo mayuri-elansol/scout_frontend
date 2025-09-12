3d666efafd5420e7d3e297494c84fb18b37fc0ec
prachi-elansol <prachij@elansoltech.com>
2025-09-12 18:47:27 +0530

    added table in alert pages

--- src/app/components/molecules/FilterPanel/FilterPanel.tsx
+++ src/app/components/molecules/FilterPanel/FilterPanel.tsx
@@ -226,7 +226,6 @@ const FilterPanel: React.FC<FilterPanelProps> = ({
               );
             })}
           </Grid>
-
           {/* Action Buttons */}
           {(showApplyButton || showClearButton) && (
             <>
