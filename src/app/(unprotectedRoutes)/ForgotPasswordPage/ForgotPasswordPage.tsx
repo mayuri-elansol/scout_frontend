bd29bde7b073ac6a35db21c46535aa777814d857
prachi-elansol <prachij@elansoltech.com>
2025-09-26 16:36:59 +0530

    remove unused variable issubmitted from reset and forget page

--- src/app/(unprotectedRoutes)/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ src/app/(unprotectedRoutes)/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -13,7 +13,6 @@ const ForgotPasswordPage: React.FC = () => {
   });
 
   const [isLoading, setIsLoading] = useState(false);
-  const [isSubmitted, setIsSubmitted] = useState(false);
   const [error, setError] = useState<string>("");
 
   // 👇 states for toggle visibility
@@ -28,7 +27,6 @@ const ForgotPasswordPage: React.FC = () => {
     try {
       // Simulate API call
       await new Promise((resolve) => setTimeout(resolve, 2000));
-      setIsSubmitted(true);
       console.log("formdata", formData);
   
     } catch (err) {
