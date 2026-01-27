b0695a0c78e7d7cffd0d923204e6ced9f48ff11b
SanikaDose <sanikad@elansoltech.com>
2026-01-27 17:29:48 +0530

    sonar

--- src/app/components/organisms/configurator/camera-management/CameraOnboardingStep/CameraOnboardingStep.tsx
+++ src/app/components/organisms/configurator/camera-management/CameraOnboardingStep/CameraOnboardingStep.tsx
@@ -315,7 +315,25 @@ const CameraOnboardingStep: React.FC<CameraOnboardingStepProps> = ({
     setIsAdding(true);
 
     try {
+      const response = await addCamera({
+        cameraIp: formData.ipAddress.trim(),
+        cameraName: formData.cameraname.trim(),
+        userName: formData.username.trim(),
+        password: formData.password.trim(),
+        RTSPport: formData.port.trim(),
+        cameraZone: zoneList.find((z) => z.id === selectedZone)?.zoneName ?? "",
+        cameraLocation:
+          locationList.find((l) => l.id === selectedLocation)?.locationName ?? "",
+
+        channel: null,
+        refreshRate: 10,
+        connectionType: "DIRECT_TO_CAMERA",
+      }).unwrap();
+
+      // ✅ NO onCameraAdd here
+
       showToast("Camera added successfully!", "success");
+
       setFormData({
         ipAddress: "",
         cameraname: "",
@@ -391,6 +409,8 @@ const CameraOnboardingStep: React.FC<CameraOnboardingStepProps> = ({
         };
 
         console.log("Final payload:", payload);
+        const response = await addCamera(payload).unwrap();
+
         // 🔥 This updates UI instantly
       }
 
