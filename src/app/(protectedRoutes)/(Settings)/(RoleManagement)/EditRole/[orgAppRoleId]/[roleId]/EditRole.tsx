edf109a56631c9614162d81cfaf4449fb558dfeb
prachi-elansol <prachij@elansoltech.com>
2026-02-03 18:22:19 +0530

    added correct import

--- src/app/(protectedRoutes)/(Settings)/(RoleManagement)/EditRole/[orgAppRoleId]/[roleId]/EditRole.tsx
+++ src/app/(protectedRoutes)/(Settings)/(RoleManagement)/EditRole/[orgAppRoleId]/[roleId]/EditRole.tsx
@@ -26,7 +26,7 @@ import {
   useGetFeaturesByOrgIdQuery,
   useUnmappedFeatureFromRoleByRoleIdMutation,
 } from "./EditRoleApi";
-import {Loader} from "@/app/components/atoms/Loader/Loader";
+import Loader from "@/app/components/atoms/Loader/Loader";
 import { getErrorMessage } from "@/utils/getErrorMessage";
 import { RoleFeature } from "./EditRole.types";
 
