70258c441c9ee0d39a5783c2391f5489552be104
Sanika Dose <sanikad@elansoltech.com>
2026-03-12 09:36:22 +0530

    pull commit

--- src/app/components/organisms/configurator/camera-management/AIConfigurationStep/AIConfigurationStep.tsx
+++ src/app/components/organisms/configurator/camera-management/AIConfigurationStep/AIConfigurationStep.tsx
@@ -498,7 +498,8 @@ const AIConfigurationStep: React.FC<AIConfigurationStepProps> = ({
       return;
     }
 
-    setFrameUrl(getCameraFeedUrl());
+    // setFrameUrl(getCameraFeedUrl());
+    setFrameUrl(`${getCameraFeedUrl()}?_t=${Date.now()}`);
 
     const interval = setInterval(() => {
       setFrameUrl(getCameraFeedUrl());
